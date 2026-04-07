6af0b6af62bf3ace866a6ad9fde90589e773c233
caiiru <caiogome125@gmail.com>
2026-04-07 15:58:40 -0300

    FEAT: Bullet VFX Follow Target Rotation

--- Assets/Scripts/S_SpellBullet.cs
+++ Assets/Scripts/S_SpellBullet.cs
@@ -10,7 +10,8 @@ public class S_SpellBullet : MonoBehaviour
     [Header("Visual Effects")]
     public GameObject onHitVFXPrefab;
     public float onHitVFXDuration = 1;
-
+    [Tooltip("Vai seguir a rotação daquilo que ele acertou, olhando para a direção da normal")]
+    public bool followCollisionRotation = false;
 
     //Refs
     Collider _collider;
@@ -36,10 +37,35 @@ public class S_SpellBullet : MonoBehaviour
 
         if (onHitVFXPrefab)
         {
-            var vfx = Instantiate(onHitVFXPrefab, transform.position, transform.rotation);
+
+            var vfx = Instantiate(onHitVFXPrefab, transform.position, GetRotation(other));
             vfx.GetComponentInChildren<VisualEffect>().Play();
             Destroy(vfx, onHitVFXDuration);
         }
         Destroy(this.gameObject);
+    }
+    void OnCollisionEnter(Collision other)
+    { 
+    }
+    Quaternion GetRotation(Collider target)
+    {
+        if (followCollisionRotation)
+        {
+            Quaternion rot;
+            float angle = Quaternion.Angle(transform.rotation, target.transform.rotation); 
+            if(angle > 90 || angle < -90)
+            {
+                rot = Quaternion.Euler(0,angle,0);
+            }
+            else
+            {
+                rot = Quaternion.Euler(0, 180+angle, 0);
+            }
+            return rot;
+        }
+        else
+        {
+            return transform.rotation;
+        }
     }
 }
